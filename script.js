67fbbd70717745683978fc6518e13504d0c751cc
Harshita222 <124767756+Harshita222@users.noreply.github.com>
2025-03-15 12:50:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+let first = prompt("Enter a first number")
+let second = prompt("Enter a second number")
+const result = (first*2)+second
+console.alert("The final result is: " + result.)
